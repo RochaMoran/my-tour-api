3966add1679e0a4d74e1e7a90383320224064abe
RochaMoran <rochamoran1@gmail.com>
2022-06-03 08:57:03 -0600

    ADD: Delete and update site

--- src/controllers/sites.controllers.ts
+++ src/controllers/sites.controllers.ts
@@ -81,95 +81,206 @@ export const createSite = async (req: Request, res: Response) => {
 
 export const getAllSites = async (_req: Request, res: Response) => {
   try {
-    const sites = await Site.find()
-  
-    if(sites) {
+    const sites = await Site.find();
+
+    if (sites) {
       return res.json({
         ok: true,
-        sites
-      })
+        sites,
+      });
     }
     return res.json({
       ok: false,
-      msg: "Aún no hay sitios por ver"
-    })
-  } catch(error) {
+      msg: "Aún no hay sitios por ver",
+    });
+  } catch (error) {
     return res.json({
       ok: false,
       msg: "Ocurrio un error al traer los sitios",
-      error
-    })
+      error,
+    });
   }
-}
+};
 
 export const getOneSite = async (req: Request, res: Response) => {
   try {
-    const site = await Site.findById(req.params.id)
-  
-    if(site) {
+    const site = await Site.findById(req.params.id);
+
+    if (site) {
       return res.json({
         ok: true,
-        site
-      })
+        site,
+      });
     }
     return res.json({
       ok: false,
-      msg: "El sitio que buscaba no se ha encontrado"
-    })
-  } catch(error) {
+      msg: "El sitio que buscaba no se ha encontrado",
+    });
+  } catch (error) {
     return res.json({
       ok: false,
       msg: "Ocurrio un error al traer el sitio",
-      error
-    })
+      error,
+    });
   }
-}
+};
 
 export const getSitesByUser = async (_req: Request, res: Response) => {
   try {
-    const sites = await Site.find({created_by: _req.params.user})
-  
-    if(sites) {
+    const sites = await Site.find({ created_by: _req.params.user });
+
+    if (sites) {
       return res.json({
         ok: true,
-        sites
-      })
+        sites,
+      });
     }
     return res.json({
       ok: false,
-      msg: "Aún no hay vinculados con este usuario"
-    })
-  } catch(error) {
+      msg: "Aún no hay vinculados con este usuario",
+    });
+  } catch (error) {
     return res.json({
       ok: false,
       msg: "Ocurrio un error al traer los sitios",
-      error
-    })
+      error,
+    });
   }
-}
+};
 
-export const searchSite = (req:Request, res:Response) => {
+export const searchSite = (req: Request, res: Response) => {
   const name = req.params.name;
 
-  Site.find({ name: { $regex: name } }, (error:any, site:any) => {
-    if (site.length > 0 && !error) {
-      return res.json({
-        ok: true,
-        site
-      });
-    }
+  Site.find(
+    { name: { $regex: name.toLowerCase(), $options: "i" } },
+    (error: any, site: any) => {
+      if (site.length > 0 && !error) {
+        return res.json({
+          ok: true,
+          sites: site,
+        });
+      }
 
-    if (error) {
+      if (error) {
+        return res.json({
+          ok: false,
+          msg: "Ha ocurrido un error al realizar la busqueda",
+          error,
+        });
+      }
+
       return res.json({
         ok: false,
-        msg: "Ha ocurrido un error al realizar la busqueda",
-        error
+        msg: "El sitio que buscas no existe",
       });
     }
+  );
+};
 
+export const deleteSite = async (req: Request, res: Response) => {
+  try {
+    const site = await Site.findOneAndDelete({ _id: req.params.id });
+
+    if (site) {
+      return res.json({
+        ok: true,
+        msg: "Sitio eliminado exitosamente",
+        site,
+      });
+    }
     return res.json({
       ok: false,
-      msg: "El sitio que buscas no existe",
+      msg: "El sitio que buscaba no se ha encontrado",
     });
-  });
+  } catch (error) {
+    return res.json({
+      ok: false,
+      msg: "Ocurrio un error al eliminar el sitio",
+      error,
+    });
+  }
+};
+
+export const updateSite = async (req: Request, res: Response) => {
+  // console.log(req.body, req.params)
+  if (req.params.id) {
+    try {
+      let img = null;
+
+      if (req.body.imgUrl) {
+        img = req.body.imgUrl;
+      } else {
+        img = await uploadAndGetImageUrl(req?.file?.path);
+      }
+
+      if (img) {
+        Site.findByIdAndUpdate(
+          { _id: req.params.id },
+          {
+            name: req.body.name,
+            description: req.body.description,
+            imgUrl: img,
+            country: req.body.country,
+            created_by: req.body.created_by,
+            covidMeasures: {
+              vaccineCovid: req.body.vaccineCovid,
+              faceMask: req.body.faceMask,
+              statusOpen: req.body.statusOpen,
+            },
+            attentionSchedules: {
+              open: req.body.open,
+              close: req.body.close,
+            },
+            location: {
+              latitude: req.body.latitude,
+              longitude: req.body.longitude,
+              latitudeDelta: req.body.latitudeDelta,
+              longitudeDelta: req.body.longitudeDelta,
+            },
+          },
+          (error, site) => {
+            if (site) {
+              return res.status(201).json({
+                ok: true,
+                site,
+              });
+            } else if (error) {
+              return res.status(500).json({
+                ok: false,
+                msg: error?.message,
+                error,
+              });
+            } else {
+              return res.status(500).json({
+                ok: false,
+                msg: "Ocurrio un error al actualizar el sitio",
+                error,
+              });
+            }
+          }
+          );
+          return;
+      } else {
+        req.file?.path && fs.unlinkSync(req.file?.path);
+  
+        return res.status(500).json({
+          ok: false,
+          message: "Ha ocurrido un error al subir la imagen",
+        });
+      }
+    } catch (error) {
+      req.file?.path && fs.unlinkSync(req.file?.path);
+
+      return res.status(500).json({
+        ok: false,
+        message: error,
+        error,
+      });
+    }
+  } else {
+    return res.json({
+      ok: false,
+      msg: "El sitio que busca no se ha encontrado",
+    });
+  }
 };
